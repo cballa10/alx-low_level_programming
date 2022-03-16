86d28a3e9dada2e7bd6434c7afdfefbafd541f1c
cballa10 <cathieballa@gmail.com>
2022-03-16 01:04:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints _putchar
@@ -15,9 +16,9 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-		 putchar(text[i]);
+		 _putchar(text[i]);
 	}
-	 putchar('\n');
+	 _putchar('\n');
 
 	return (0);
 }
