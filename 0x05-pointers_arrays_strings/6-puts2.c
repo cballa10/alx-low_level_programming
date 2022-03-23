cfd8a801c018dfd87efc3b3ae82bd40a940ad90e
cballa10 <99319960+cballa10@users.noreply.github.com>
2022-03-23 17:16:23 +0300

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,8 +4,9 @@
 * puts2 - prints one char out of 2 of a string
 * followed by a new line
 * @str: string to print the chars from
+* Return: Always 0
 */
-void puts2(char *str);
+void puts2(char *str)
 {
 int len, i;
 
