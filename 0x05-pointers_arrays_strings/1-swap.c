a8e5719eb53b372e1bb01de863e83311fefc47da
cballa10 <99319960+cballa10@users.noreply.github.com>
2022-03-23 15:35:03 +0300

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - function that swaps the value of two intergers
+* @a: the pointer to first value
+* @b: the pointer to second value
+* Return: Always 0
+*/
+void swap_int(int *a, int *b)
+{
+     int swap;
+     
+     swap = *a;
+     *a = *b;
+     *b = swap;
+}
