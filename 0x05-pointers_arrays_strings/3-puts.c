7db2a26702f3672b670f2f99d4dc83c6377793fb
cballa10 <99319960+cballa10@users.noreply.github.com>
2022-03-23 15:53:43 +0300

    puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - prints a string to std out
+* @str: pointer to the string to print
+*Return: Always 0
+*/
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
