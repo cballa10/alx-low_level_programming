0bc7923d04b3206977795e0c1e05b01a60104d7f
cballa10 <99319960+cballa10@users.noreply.github.com>
2022-03-28 13:24:50 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,18 +2,18 @@
 
 /**
 * _strchr - locates a character in a string
-* @s: string to search 
-* @c: char to find
-* Return: a pointer to the first occurence of the character 
-* c in the string, or NULL if the charcter is not found
-*/
-
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
 char *_strchr(char *s, char c)
 {
 int a;
 
-while (1);
-{  
+while (1)
+{
 a = *s++;
 if (a == c)
 {
